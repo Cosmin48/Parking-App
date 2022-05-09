da834cb90e30bf20bf2907a8271e002a4c004cd9
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2022-05-09 18:29:57 +0300

    add fxml

--- Desktop/demo/src/main/java/com/example/demo/HelloController.java
+++ Desktop/demo/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+    public void switchtoScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchtoScene2(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("bye-view.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
