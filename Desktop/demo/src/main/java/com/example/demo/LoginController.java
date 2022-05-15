233691e18f93f1df2e0b27a6aa3a235891da1088
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2022-05-15 22:31:14 +0300

    PA-5 added login button

--- Desktop/demo/src/main/java/com/example/demo/LoginController.java
+++ Desktop/demo/src/main/java/com/example/demo/LoginController.java
@@ -1,12 +1,28 @@
 package com.example.demo;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import javafx.scene.control.Button;
 
+import java.io.File;
+import java.util.ResourceBundle;
+import java.net.URL;
+
 public class LoginController {
+    @FXML
     private Button cancelButton;
+
+    @FXML
+    private Label loginMessageLabel;
+    public void loginButtonOnAction(ActionEvent event){
+        loginMessageLabel.setText("You try to login");
+    }
     public void cancelButtonOnAction(ActionEvent event){
         Stage stage = (Stage)cancelButton.getScene().getWindow();
         stage.close();
