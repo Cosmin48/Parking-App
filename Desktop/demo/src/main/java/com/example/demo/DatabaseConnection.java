9860c6bc03d11126728084a4ec9e94254656ca60
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2022-05-16 00:44:40 +0300

    PA-5 connect database

---
+++ Desktop/demo/src/main/java/com/example/demo/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+    public Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/amitdb";
+        String user = "root";
+        String password = "FCBarcelona14";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+        return databaseLink;
+    }
+}
--- Desktop/demo/src/main/java/com/example/demo/LoginController.java
+++ Desktop/demo/src/main/java/com/example/demo/LoginController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -11,6 +13,9 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.net.URL;
 
@@ -20,11 +25,44 @@ public class LoginController {
 
     @FXML
     private Label loginMessageLabel;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
+    @FXML
+    private TextField enterCardNumberField;
+
     public void loginButtonOnAction(ActionEvent event){
-        loginMessageLabel.setText("You try to login");
+        if (!usernameTextField.getText().isBlank() && !enterPasswordField.getText().isBlank()){
+            validateLogin();
+        } else {
+            loginMessageLabel.setText("Please enter username and password");
+        }
     }
     public void cancelButtonOnAction(ActionEvent event){
         Stage stage = (Stage)cancelButton.getScene().getWindow();
         stage.close();
+    }
+    public void validateLogin(){
+       DatabaseConnection connectNow=new DatabaseConnection();
+        Connection connectDB=connectNow.getConnection();
+        String verifyLogin = "SElECT count(1) FROM user_account WHERE username = '" + usernameTextField.getText() + "' AND password ='" + enterPasswordField.getText() + "' AND card_number ='" + enterCardNumberField.getText() +"'";
+        try{
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+            while(queryResult.next()){
+                if (queryResult.getInt(1)==1) {
+                   loginMessageLabel.setText("Welcome!");
+                } else {
+                    loginMessageLabel.setText("Invalid login. Please try again!");
+                    usernameTextField.setText("");
+                    enterPasswordField.setText("");
+                    enterCardNumberField.setText("");
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
     }
 }
