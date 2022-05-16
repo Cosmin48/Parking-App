8c72556f3a2487af185b8903958d82b00a5d65fd
Stroiescu Cosmin <cosmin.stroiescu@student.upt.ro>
2022-05-16 20:27:00 +0300

    PA-91 added register user method

--- Desktop/demo/src/main/java/com/example/demo/RegisterController.java
+++ Desktop/demo/src/main/java/com/example/demo/RegisterController.java
@@ -24,17 +24,35 @@ public class RegisterController {
     private Button closeButton;
     @FXML
     private Label registrationMessageLabel;
+    @FXML
+    private PasswordField setPasswordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private Label confirmPasswordLabel;
     private Parent root;
     private Stage stage;
     private Scene scene;
 
     public void registerButtonOnAction(ActionEvent event){
         registrationMessageLabel.setText("User has been registered succesfully!");
+        registerUser();
     }
 
     public void closeButtonOnAction(ActionEvent event){
         Stage stage=(Stage)closeButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void registerUser(){
+
+        if (setPasswordField.getText().equals(confirmPasswordField.getText())){
+            confirmPasswordLabel.setText("You are set");
+
+        }else{
+            confirmPasswordLabel.setText("Password does not match");
+
+        }
     }
     public void switchToLogin(ActionEvent event) throws IOException {
          root = FXMLLoader.load(getClass().getResource("login.fxml"));
