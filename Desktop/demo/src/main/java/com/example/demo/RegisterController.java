e8886d5a6e44a77a6b2a2df9280684297c5218bf
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2022-05-16 20:09:27 +0300

    PA-91 add switchToLogin method

--- Desktop/demo/src/main/java/com/example/demo/RegisterController.java
+++ Desktop/demo/src/main/java/com/example/demo/RegisterController.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -9,6 +13,7 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -17,11 +22,20 @@ import java.sql.Statement;
 public class RegisterController {
     @FXML
     private Button closeButton;
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
 
     public void closeButtonOnAction(ActionEvent event){
         Stage stage=(Stage)closeButton.getScene().getWindow();
         stage.close();
     }
-
+    public void switchToLogin(ActionEvent event) throws IOException {
+         root = FXMLLoader.load(getClass().getResource("login.fxml"));
+         stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+         scene=new Scene(root);
+         stage.setScene(scene);
+         stage.show();
+    }
 
 }
