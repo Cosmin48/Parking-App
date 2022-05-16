d458be575168f60c414deb887e43ca7d8e66d777
Stroiescu Cosmin <cosmin.stroiescu@student.upt.ro>
2022-05-16 19:31:16 +0300

    added closeButton method

--- Desktop/demo/src/main/java/com/example/demo/RegisterController.java
+++ Desktop/demo/src/main/java/com/example/demo/RegisterController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
@@ -13,6 +15,13 @@ import java.sql.Statement;
 
 
 public class RegisterController {
+    @FXML
+    private Button closeButton;
+
+    public void closeButtonOnAction(ActionEvent event){
+        Stage stage=(Stage)closeButton.getScene().getWindow;
+        Stage.close();
+    }
 
 
 }
