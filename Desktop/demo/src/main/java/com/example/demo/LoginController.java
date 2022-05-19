50da1f1c90102065b5f9524e4dd7efc281bd178a
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2022-05-20 01:34:36 +0300

    PA-6 main file

--- Desktop/demo/src/main/java/com/example/demo/LoginController.java
+++ Desktop/demo/src/main/java/com/example/demo/LoginController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,6 +18,7 @@ import javafx.scene.control.Button;
 import javafx.stage.StageStyle;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -35,10 +37,13 @@ public class LoginController {
     private PasswordField enterPasswordField;
     @FXML
     private TextField enterCardNumberField;
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
 
     public void loginButtonOnAction(ActionEvent event){
         if (!usernameTextField.getText().isBlank() && !enterPasswordField.getText().isBlank()){
-            validateLogin();
+            validateLogin(event);
         } else {
             loginMessageLabel.setText("Please enter username and password");
         }
@@ -47,7 +52,7 @@ public class LoginController {
         Stage stage = (Stage)cancelButton.getScene().getWindow();
         stage.close();
     }
-    public void validateLogin(){
+    public void validateLogin(ActionEvent event){
        DatabaseConnection connectNow=new DatabaseConnection();
         Connection connectDB=connectNow.getConnection();
         String verifyLogin = "SElECT count(1) FROM user_account WHERE username = '" + usernameTextField.getText() + "' AND password ='" + enterPasswordField.getText() + "' AND card_number ='" + enterCardNumberField.getText() +"'";
@@ -56,12 +61,9 @@ public class LoginController {
             ResultSet queryResult = statement.executeQuery(verifyLogin);
             while(queryResult.next()){
                 if (queryResult.getInt(1)==1) {
-                   loginMessageLabel.setText("Welcome!");
+                   switchToMain(event);
                 } else {
                     loginMessageLabel.setText("Invalid login. Please try again!");
-                    usernameTextField.setText("");
-                    enterPasswordField.setText("");
-                    enterCardNumberField.setText("");
                 }
             }
         }catch (Exception e) {
@@ -69,17 +71,11 @@ public class LoginController {
             e.getCause();
         }
     }
-    public void createAccountForm(){
-        try{
-            Parent root= FXMLLoader.load(getClass().getResource("register.fxml"));
-            Stage registerStage=new Stage();
-            registerStage.initStyle(StageStyle.UNDECORATED);
-            registerStage.setScene(new Scene(root, 520 ,550));
-            registerStage.show();
-
-        }catch(Exception e){
-            e.printStackTrace();
-            e.getCause();
-        }
+    public void switchToMain(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
