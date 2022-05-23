bf18b9c04e231a408875d63dd7dcac3d8c723a93
Bogdans2001 <102230164+Bogdans2001@users.noreply.github.com>
2022-05-23 23:30:29 +0300

    PA-16 creating 2 fxml Files

---
+++ Desktop/demo/src/main/java/com/example/demo/RateCityAdminController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RateCityAdminController {
+    private Parent parent;
+    private Scene scene;
+    private Stage stage;
+
+
+}
