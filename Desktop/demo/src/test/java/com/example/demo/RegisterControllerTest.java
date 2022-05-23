82a360e6361ca29bb253fe0032193e081f78d7e9
Stroiescu Cosmin <cosmin.stroiescu@student.upt.ro>
2022-05-23 23:48:25 +0300

    added jacoco maven plugin

--- Desktop/demo/src/test/java/com/example/demo/RegisterControllerTest.java
+++ Desktop/demo/src/test/java/com/example/demo/RegisterControllerTest.java
@@ -21,6 +21,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import static com.mysql.cj.conf.PropertyKey.PASSWORD;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterControllerTest extends RegisterController {
@@ -50,37 +51,34 @@ class RegisterControllerTest extends RegisterController {
 
     private String getEncryptedpassword(String password) {
         String encryptedpassword = null;
-        try
-        {
+        try {
             MessageDigest m = MessageDigest.getInstance("MD5");
             m.update(password.getBytes());
 
             byte[] bytes = m.digest();
 
             StringBuilder s = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
+            for (int i = 0; i < bytes.length; i++) {
                 s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
             }
 
             encryptedpassword = s.toString();
-        }
-        catch (
-                NoSuchAlgorithmException e)
-        {
+        } catch (
+                NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return encryptedpassword;
     }
-    public void findInCard_database(String card_number, ActionEvent event){
-        String querry="SELECT count(1) from card_numbers WHERE card_numbers='"+card_number+"'";
-        DatabaseConnection connectNow=new DatabaseConnection();
-        Connection connectDB=connectNow.getConnection();
-        try{
+
+    public void findInCard_database(String card_number, ActionEvent event) {
+        String querry = "SELECT count(1) from card_numbers WHERE card_numbers='" + card_number + "'";
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+        try {
             Statement statement = connectDB.createStatement();
             ResultSet queryResult = statement.executeQuery(querry);
-            while(queryResult.next()){
-                if (queryResult.getInt(1)==1) {
+            while (queryResult.next()) {
+                if (queryResult.getInt(1) == 1) {
                     registerUser();
                     confirmPasswordLabel.setText("");
                     switchToSuccess(event);
@@ -88,16 +86,17 @@ class RegisterControllerTest extends RegisterController {
                     registrationMessageLabel.setText("Card number not found");
                 }
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
         }
     }
-    public void registerButtonOnAction(ActionEvent event) throws IOException {
-        if (setPasswordField.getText().equals(confirmPasswordField.getText())){
-            findInCard_database(cardnumberTextField.getText(),event);
 
-        }else{
+    public void registerButtonOnAction(ActionEvent event) throws IOException {
+        if (setPasswordField.getText().equals(confirmPasswordField.getText())) {
+            findInCard_database(cardnumberTextField.getText(), event);
+
+        } else {
             registrationMessageLabel.setText("");
             confirmPasswordLabel.setText("Password does not match");
 
@@ -105,49 +104,51 @@ class RegisterControllerTest extends RegisterController {
 
     }
 
-    public void closeButtonOnAction(ActionEvent event){
-        Stage stage=(Stage)closeButton.getScene().getWindow();
+    public void closeButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
         Platform.exit();
     }
 
-    public void registerUser(){
-        DatabaseConnection connectNow=new DatabaseConnection();
-        Connection connectDB=connectNow.getConnection();
+    public void registerUser() {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
 
-        String firstname=firstnameTextField.getText();
-        String lastname=lastnameTextField.getText();
-        String username=usernameTextField.getText();
-        String password=setPasswordField.getText();
-        String card_number=cardnumberTextField.getText();
+        String firstname = firstnameTextField.getText();
+        String lastname = lastnameTextField.getText();
+        String username = usernameTextField.getText();
+        String password = setPasswordField.getText();
+        String card_number = cardnumberTextField.getText();
 
 
-        String insertFields= "INSERT INTO user_account (firstname, lastname, username, password, card_number) VALUES('";
-        String insertValues=firstname+ "','" +lastname+ "','"+username+ "','"+ getEncryptedpassword(password) +"','" +card_number+ "')";
-        String insertToRegister=insertFields+ insertValues;
+        String insertFields = "INSERT INTO user_account (firstname, lastname, username, password, card_number) VALUES('";
+        String insertValues = firstname + "','" + lastname + "','" + username + "','" + getEncryptedpassword(password) + "','" + card_number + "')";
+        String insertToRegister = insertFields + insertValues;
 
-        try{
-            Statement statement=connectDB.createStatement();
+        try {
+            Statement statement = connectDB.createStatement();
             statement.executeUpdate(insertToRegister);
             registrationMessageLabel.setText("User has been registered successfully!");
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
         }
 
 
     }
+
     public void switchToSuccess(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("success.fxml"));
-        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        scene=new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
     public void switchToLogin(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-        scene=new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
@@ -164,9 +165,6 @@ class RegisterControllerTest extends RegisterController {
     void closeButtonOnAction() {
     }
 
-    @Test
-    void registerUser() {
-    }
 
     @Test
     void switchToSuccess() {
