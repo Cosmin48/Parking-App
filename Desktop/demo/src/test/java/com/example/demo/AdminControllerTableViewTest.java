7ea712b98ea964f0dcf65428d49e1230e2b522a1
Stroiescu Cosmin <cosmin.stroiescu@student.upt.ro>
2022-05-23 23:53:10 +0300

    generating methods for AdminControllerTableView

---
+++ Desktop/demo/src/test/java/com/example/demo/AdminControllerTableViewTest.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTableViewTest {
+
+    @Test
+    void getFirstname() {
+    }
+
+    @Test
+    void getLastname() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void getImage() {
+    }
+
+    @Test
+    void setFirstname() {
+    }
+
+    @Test
+    void setLastname() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void setImage() {
+    }
+}
