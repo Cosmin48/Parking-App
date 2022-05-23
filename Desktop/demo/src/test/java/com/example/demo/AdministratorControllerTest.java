f18ceb16a1b11f077e143dc2d619d9fd3d49820f
Stroiescu Cosmin <cosmin.stroiescu@student.upt.ro>
2022-05-24 00:14:25 +0300

    generating tests methods for administrator controller

---
+++ Desktop/demo/src/test/java/com/example/demo/AdministratorControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdministratorControllerTest {
+
+    @Test
+    void switchTocheckRequest() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void cancelButtonOnAction() {
+    }
+
+    @Test
+    void acceptButtonOnAction() {
+    }
+
+    @Test
+    void denyButtonOnAction() {
+    }
+}
