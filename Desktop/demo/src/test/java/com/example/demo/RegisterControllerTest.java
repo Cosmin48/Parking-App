134c6e73bef720c69b00e96ce9556edbc95cb2d2
Stroiescu Cosmin <cosmin.stroiescu@student.upt.ro>
2022-05-23 23:34:27 +0300

    added methods for testing register controller

---
+++ Desktop/demo/src/test/java/com/example/demo/RegisterControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterControllerTest {
+
+    @Test
+    void findInCard_database() {
+    }
+
+    @Test
+    void registerButtonOnAction() {
+    }
+
+    @Test
+    void closeButtonOnAction() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void switchToSuccess() {
+    }
+
+    @Test
+    void switchToLogin() {
+    }
+}
